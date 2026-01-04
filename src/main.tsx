b4cc294238ce18f1950e85021116decb3bf2c834
bruno.picolo <bruno.picolo@edataconsulting.de>
2026-01-04 12:49:36 +0000

    task: github pages needs hashRouter instead of browserrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "@tenant-theme";
 import "./index.css";
@@ -15,8 +15,8 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
